da3f5f7cc56477a1d4757106836dbd13c2b7528c
JEET RAHA <jeetrahaaccademic@gmail.com>
2025-02-12 23:30:40 +0530

    add code 1

---
+++ ArrayBasic1.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        // initializing array by values
+        int[] arr = { 13, 82, 90, 20, 21 };
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
