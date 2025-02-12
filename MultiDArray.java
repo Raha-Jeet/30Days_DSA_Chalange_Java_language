b39c382ceafee93b4cdfc0cd28a6d1d04244863e
JEET RAHA <jeetrahaaccademic@gmail.com>
2025-02-12 23:46:18 +0530

    add array codes

--- MultiDArray.java
+++ MultiDArray.java
@@ -4,7 +4,9 @@ public class MultiDArray {
 public static void main(String args[])
 {
     // declaring and initializing 2D array
-    int arr[][] = { { 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } };
+    int arr[][] = { { 2, 7, 9 },
+                    { 3, 6, 1 }, 
+                    { 7, 4, 2 } };
 
     // printing 2D array
     for (int i = 0; i < 3; i++) {
